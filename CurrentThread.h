a2575dd1a565ec66df81bea8b973bf776e2749e9
tingyufu612-wq <tingyufu612@gmail.com>
2026-08-30 21:00:20 -0700

    modify

--- CurrentThread.h
+++ CurrentThread.h
@@ -1,19 +1,20 @@
 #pragma once
 
-#include <sys/syscall.h>
 #include <unistd.h>
+#include <sys/syscall.h>
+
 namespace CurrentThread
 {
-   extern __thread int  t_cachedTId;
+    extern __thread int t_cachedTid;
 
     void cacheTid();
 
     inline int tid()
     {
-        if(__builtin_expect(t_cachedTid==0,0))
+        if (__builtin_expect(t_cachedTid == 0, 0))
         {
             cacheTid();
         }
         return t_cachedTid;
     }
-};
+}
