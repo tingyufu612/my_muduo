5b4e33e1eb874405af214b8139a22aa1f4c8c556
tingyufu612-wq <tingyufu612@gmail.com>
2026-08-30 22:12:54 -0700

    modify

--- Socket.cc
+++ Socket.cc
@@ -1,14 +1,13 @@
 #include "Socket.h"
-#include "InetAddress.h"
 #include "Logger.h"
+#include "InetAddress.h"
 
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/types.h>         
 #include <sys/socket.h>
 #include <strings.h>
 #include <netinet/tcp.h>
-
-
+#include <string.h> 
 
 Socket::~Socket()
 {
@@ -17,28 +16,33 @@ Socket::~Socket()
 
 void Socket::bindAddress(const InetAddress &localaddr)
 {
-    if(0!=bind(sockfd_,(sockaddr*)localaddr.getSockAddr(),sizeof(sockaddr_in)));
+    if (0 != ::bind(sockfd_, (sockaddr*)localaddr.getSockAddr(), sizeof(sockaddr_in)))
     {
-        LOG_FATAL("bind sockfd:%d fail \n",sockfd_);
-
+        LOG_FATAL("bind sockfd:%d failreason: %s \n", sockfd_, strerror(errno));
     }
 }
 
 void Socket::listen()
 {
-    if(0!=::listen(sockfd_,1024))
+    if (0 != ::listen(sockfd_, 1024))
     {
-        LOG_FATAL("listen sockfd:%d fail \n",sockfd_);
+        LOG_FATAL("listen sockfd:%d fail \n", sockfd_);
     }
 }
 
 int Socket::accept(InetAddress *peeraddr)
 {
+    /**
+     * 1. accept函数的参数不合法
+     * 2. 对返回的connfd没有设置非阻塞
+     * Reactor模型 one loop per thread
+     * poller + non-blocking IO
+     */ 
     sockaddr_in addr;
-    socklen_t len;
-    bzero(&addr,sizeof addr);
-    int connfd=::accept(sockfd_,(sockaddr*)&addr,&len);
-    if(connfd>=0)
+    socklen_t len = sizeof addr;
+    bzero(&addr, sizeof addr);
+    int connfd = ::accept4(sockfd_, (sockaddr*)&addr, &len, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    if (connfd >= 0)
     {
         peeraddr->setSockAddr(addr);
     }
@@ -47,31 +51,32 @@ int Socket::accept(InetAddress *peeraddr)
 
 void Socket::shutdownWrite()
 {
-    if(::shutdown(sockfd_,SHUT_WR))
+    if (::shutdown(sockfd_, SHUT_WR) < 0)
     {
-        LOG_FATAL("shutdownWrite error");
+        LOG_ERROR("shutdownWrite error");
     }
 }
 
 void Socket::setTcpNoDelay(bool on)
 {
-    int optval=on?1:0;
-    ::setsockopt(sockfd_,IPPROTO_TCP,TCP_NODELAY, &optval,sizeof optval);
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof optval);
 }
 
 void Socket::setReuseAddr(bool on)
 {
-    int optval=on?1:0;
-    ::setsockopt(sockfd_,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof optval);
-}
-void Socket::setReusePort(bool on)
-{
-    int optval=on?1:0;
-    ::setsockopt(sockfd_,SOL_SOCKET,SO_REUSEPORT,&optval,sizeof optval);
-}
-void Socket::setKeepAlive(bool on)
-{
-    int optval=on?1:0;
-    ::setsockopt(sockfd_,SOL_SOCKET,SO_KEEPALIVE,&optval,sizeof optval);
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
 }
 
+void Socket::setReusePort(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof optval);
+}
+
+void Socket::setKeepAlive(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof optval);
+}
